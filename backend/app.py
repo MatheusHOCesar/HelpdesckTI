f84543faddd3a5ae39481b0010f1a85ad91408f1
Matheus Cesar <141744903+MatheusHOCesar@users.noreply.github.com>
2026-09-08 23:25:21 -0300

    api se conecta ao mysql e listagem de chamados

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,36 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+import mysql.connector
+from mysql.connector import Error
 
 app = Flask(__name__)
 
-@app.route('/ping', methods=['GET'])
-def ping():
-    return jsonify({"status": "API em construcao"}), 200
+def get_db_connection():
+    try:
+        conn = mysql.connector.connect(
+            host= '10.20.30.3',
+            database= 'helpdesck_db',
+            user= 'api_user',
+            password= 'senhabanco123'
+        )
+        return conn
+    except Error as e:
+        print(f"Erro de banco: {e}")
+        return None
+    
+@app.route('/api/chamados', methods=['GET'])
+def listar_chamados():
+    conn = get_db_connection()
+    if not conn:
+        return jsonify({"erro": "Banco indisponivel"}), 500
+
+    try:
+        cursor = conn.cursor(dictionary= True)
+        cursor.execute("SELECT FROM chamados ORDER BY data_criacao DESC")
+        chamados = cursor.fetchall()
+        return jsonify(chamados), 200
+    finally:
+        cursor.close()
+        conn.close()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
